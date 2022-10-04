d02993e8556bc16c9291ee6635719c26c6af3d0b
Ianportods <ian.silva@fatec.sp.gov.br>
2022-10-04 09:41:30 -0300

    flask

--- JEAN/desafio3/app.py
+++ JEAN/desafio3/app.py
@@ -3,10 +3,18 @@ from flask import Flask,render_template,request,flash,redirect,url_for
 
 app = Flask(__name__)
 
-@app.contato('/contato/')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contato')
 def contato():
-    if email != "":
-        return render_template('index.html')
+    return render_template("contato.html")
+
+@app.route('/quemsomos')
+def quemsomos():
+    return render_template("quemsomos.html")
+
 
 
 app.run(debug=True)
